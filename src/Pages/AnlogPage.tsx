5564b966e05086f1a407574590bde1b18e331a8e
jacob-kourieh <you@example.com>
2022-05-18 15:03:44 +0200

    #11 fixa importera svg/png

--- src/Pages/AnlogPage.tsx
+++ src/Pages/AnlogPage.tsx
@@ -1,10 +1,75 @@
 import React from 'react'
+import useTimer from 'easytimer-react-hook';
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-function AnlogPage() {
+
+interface times {
+    countdown?: boolean,
+    seconds?: number,
+    minutes?: number,
+    hours?: number
+}
+
+
+
+const AnlogPage: React.FC<times> = () => {
+
+    const location: object | any = useLocation();
+
+    const time: object | any = location.state;
+
+    const [timer] = useTimer({
+        startValues: {
+            hours: time.hours,
+            minutes: time.minutes,
+            seconds: time.seconds
+        }, countdown: true,
+    });
+
+    const sec: number = timer.getTimeValues().seconds
+    const min: number = timer.getTimeValues().minutes
+    const hr: number = timer.getTimeValues().hours
+    const [digitalTime, setdigitalTime] = useState<string>("timerShown")
+
+
+    function start() {
+        timer.start();
+    };
+
+    function pause() {
+        timer.pause();
+
+    };
+
+    function stop() {
+        timer.stop();
+    };
+
+    function reset() {
+        window.location.reload()
+    };
+
+
+
+
     return (
-        <div>
+        <section className="digital-container">
 
-        </div>
+
+            <article>
+                <p className={digitalTime} >{timer.getTimeValues().toString()}</p>
+            </article>
+
+
+            <button onClick={() => start()}>start</button>
+            <button onClick={() => pause()}>pause</button>
+            <button onClick={() => stop()}>stop</button>
+            <button onClick={() => reset()}>reset</button>
+
+
+        </section>
+
     );
 }
 
